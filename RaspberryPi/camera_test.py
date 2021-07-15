35777bb21e8574c253ca4ad983f4297be55e466d
Rishi Pathak <rpathak38@gatech.edu>
2021-07-15 18:52:03 -0500

    Resized Image for Raspberry Pi

--- RaspberryPi/camera_test.py
+++ RaspberryPi/camera_test.py
@@ -4,7 +4,8 @@ cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     retVal, frame = cap.read()
-    frame = cv2.flip(frame, 0);
+    frame = cv2.flip(frame, 0)
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
     if retVal:
         cv2.imshow("frame", frame)
